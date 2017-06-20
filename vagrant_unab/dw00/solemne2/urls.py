2a860809206cec2c73517db377e60a84e6a57c8b
Unknown <jtomas.at@gmail.com>
2017-06-20 18:11:05 -0400

    cargar entorno virtual
    y contenido dentro en django

---
+++ vagrant_unab/dw00/solemne2/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from solemne2 import views
+urlpatterns = [
+    url(r'^$', views.index),
+    #url(r'^new/$', views.movie_form, name='movie_create'),
+    #url(r'^edit/(?P<pk>\d+)$', views.movie_update, name='movie_edit'),
+    #url(r'^delete/(?P<pk>\d+)$', views.movie_delete, name='movie_delete'),
+
+]

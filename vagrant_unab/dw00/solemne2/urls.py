d4bdcc4029477791a265faa700328da0dcb15e1b
jtomas23 <Tomas@MacBook-Pro-de-Jose.local>
2017-06-22 17:28:43 -0400

    Despliegue de Models
    se despliega las noticias desde modelo. Se agrega “categoría” a la BD

--- vagrant_unab/dw00/solemne2/urls.py
+++ vagrant_unab/dw00/solemne2/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from solemne2 import views
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
     #url(r'^new/$', views.movie_form, name='movie_create'),
     #url(r'^edit/(?P<pk>\d+)$', views.movie_update, name='movie_edit'),
     #url(r'^delete/(?P<pk>\d+)$', views.movie_delete, name='movie_delete'),
--- vagrant_unab/dw00/solemne2/views.py
+++ vagrant_unab/dw00/solemne2/views.py
@@ -2,7 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from solemne2.models import Post
 
 # Create your views here.
 def index(request):
-  return render(request, 'index.html', {})
+  #return render(request, 'index.html', {})
+	data = {}
+	data['object_list'] = Post.objects.all().order_by('-id')
+	template_name = 'index.html'
+	return render(request, template_name, data)

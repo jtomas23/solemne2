f36ef7def29e398fb28803d5582d888423226458
jtomas23 <Tomas@MacBook-Pro-de-Jose.local>
2017-06-22 20:57:08 -0400

    Detalle de noticias
    Detallle de noticias ya se genera dinámicamente

--- vagrant_unab/dw00/solemne2/views.py
+++ vagrant_unab/dw00/solemne2/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from solemne2.models import Post
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+
 
 # Create your views here.
 def index(request):
@@ -13,11 +15,8 @@ def index(request):
 	return render(request, template_name, data)
 
 def noticia_detalle(request, pk):
-  #return render(request, 'index.html', {})
 	#data = {}
 	#data['object_list'] = Post.objects.all().order_by('-id')
+	posts = get_object_or_404(Post, pk=pk)
 	template_name = 'noticia_detalle.html'
-	data = get_object_or_404(Post, pk=pk)
-
-
-	return render(request, template_name, data)
+	return render(request, template_name,{'posts': posts})
